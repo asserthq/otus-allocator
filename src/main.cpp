1e4fe9b1fc5d7dc9600f5e9a7efe40c22bdcb6c9
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-08-06 23:36:40 +0700

    test stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include "allocator.h"
+#include <vector>
+
+struct hard 
+{
+    int fa;
+    int fi;
+    hard(int fa, int fi) : fa(fa), fi(fi) {};
+};
+
 
 int main()
 {
+    std::vector<int, my::allocator<int>> v = { 1, 2, 3, 4 };
+    v.push_back(777);
 
     return 0;
 }
